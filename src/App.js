edbaf423b885c63c1e63beaeaefce8cb388d1870
Arnav Borborah <arnavborborah11@gmail.com>
2020-09-06 13:05:12 -0400

    Create bulma navbar

--- src/App.js
+++ src/App.js
@@ -3,23 +3,54 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Apply from "./pages/apply";
+import "./App.sass";
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About Us</Link>
-            </li>
-            <li>
-              <Link to="/apply">Apply to the Team</Link>
-            </li>
-          </ul>
+        <nav className="navbar">
+          <div className="navbar-brand">
+            <a href="/" className="navbar-item">
+              <img src="https://bulma.io/images/bulma-logo.png" alt="Logo" />
+            </a>
+            <a
+              role="button"
+              className="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div id="navbar" className="navbar-menu">
+            <div class="navbar-start">
+              <Link className="navbar-item" to="/">
+                Home
+              </Link>
+              <Link className="navbar-item" to="/about">
+                About Us
+              </Link>
+              <Link className="navbar-item" to="/apply">
+                Join the team!
+              </Link>
+            </div>
+            <div className="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <a class="button is-primary" href="/">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a class="button is-light" href="/">
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </nav>
         <Switch>
           <Route path="/apply">

29332254e71bab53bef04ff34b5040f6a6b6a711
Arnav Borborah <arnavborborah11@gmail.com>
2020-09-06 22:26:47 -0400

    Start building about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function About() {
-  return <h2>About</h2>;
+  return (
+    <>
+      <section className="hero is-info">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">About the Team</h1>
+            <h2 className="subtitle">
+              Meet the hardworking individuals who drive InvisionAR
+            </h2>
+          </div>
+        </div>
+      </section>
+      <section className="section"></section>
+    </>
+  );
 }

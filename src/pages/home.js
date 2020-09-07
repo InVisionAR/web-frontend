d082b1197c5effc147b1dcc7bb3dd25fded8fad1
Rohan Kewalramani <rkewalramani4@gmail.com>
2020-09-07 14:38:18 -0400

    Moved CSS to external file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,43 +1,15 @@
 import React from "react";
-
-{/* Styling Start */ }
-
-const body = {
-  backgroundColor: "#2B2D42",
-};
-
-const title = {
-  textAlign: "center",
-  color: "#D90429",
-  fontSize: "80px"
-};
-
-const slogan = {
-  color: "#EF233C",
-  textAlign: "center",
-  fontSize: "30px"
-};
-
-const start_btn = {
-  backgroundColor: "#8D99AE",
-  color: "#EDF2F4",
-  fontSize: "20px",
-  justifyContent: "center",
-  alignSelf: "center",
-  alignItems: "center"
-};
-
-{/* Styling End */ }
+import "./style.css";
 
 export default function Home() {
   return (
     <>
       <section className="hero is-link is-fullheight-with-navbar">
-        <div className="hero-body" style={body}>
+        <div className="hero-body body">
           <div className="container">
-            <h1 className="title" style={title}>InvisionAR</h1>
-            <p style={slogan}>InVisioning the future...</p>
-            <button class="button is-primary" to="#" style={start_btn}><strong>GET STARTED</strong></button>
+            <h1 className="title" id="title">InvisionAR</h1>
+            <p className="slogan">InVisioning the future...</p>
+            <button class="button is-primary" id="start-btn" to="#"><strong>GET STARTED</strong></button>
           </div>
         </div>
       </section>

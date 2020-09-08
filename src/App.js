c2744cc727bf555b0013a46259ce4bcb8afc07a8
Arnav Borborah <arnavborborah11@gmail.com>
2020-09-07 22:33:56 -0400

    Pull updated navbar from feature/landing-page

--- src/App.js
+++ src/App.js
@@ -9,62 +9,60 @@ import Apply from "./pages/apply";
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav className="navbar is-fixed-top">
-          <div className="navbar-brand">
-            <a href="/" className="navbar-item">
-              <img src="https://bulma.io/images/bulma-logo.png" alt="Logo" />
-            </a>
-            <a
-              role="button"
-              className="navbar-burger burger"
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="navbarBasicExample"
-            >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </a>
+      <nav className="navbar is-fixed-top">
+        <div className="navbar-brand">
+          <a href="/" className="navbar-item">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Logo" />
+          </a>
+          <a
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div id="navbar" className="navbar-menu">
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/">
+              Home
+            </Link>
+            <Link className="navbar-item" to="/about">
+              About Us
+            </Link>
+            <Link className="navbar-item" to="/apply">
+              Join the team!
+            </Link>
           </div>
-          <div id="navbar" className="navbar-menu">
-            <div className="navbar-start">
-              <Link className="navbar-item" to="/">
-                Home
-              </Link>
-              <Link className="navbar-item" to="/about">
-                About Us
-              </Link>
-              <Link className="navbar-item" to="/apply">
-                Join the team!
-              </Link>
-            </div>
-            <div className="navbar-end">
-              <div className="navbar-item">
-                <div className="buttons">
-                  <a className="button is-primary" href="/">
-                    <strong>Sign up</strong>
-                  </a>
-                  <a className="button is-light" href="/">
-                    Log in
-                  </a>
-                </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary" href="/">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-light" href="/">
+                  Log in
+                </a>
               </div>
             </div>
           </div>
-        </nav>
-        <Switch>
-          <Route path="/apply">
-            <Apply />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/apply">
+          <Apply />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   );
 }

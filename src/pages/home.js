80b097a3999d31c1367059ea1f53567fdb2ebd05
Rohan Kewalramani <rkewalramani4@gmail.com>
2020-09-12 14:11:54 -0400

    Changed css file name to home.css

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,21 @@
 import React from "react";
+import "./home.css";
+import logo from "./img/logo.png";
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <>
       <section className="hero is-link is-fullheight-with-navbar">
-        <div className="hero-body">
+        <div className="hero-body body">
           <div className="container">
-            <p className="title">InvisionAR</p>
+            <img src={logo} alt="Logo" class="logo-img" />
+            <h1 className="title" id="title">InvisionAR</h1>
+            <p className="slogan">InVisioning the future...</p>
+            <Link to="/about"><button class="button is-primary" id="start-btn" to="#"><strong>GET STARTED</strong></button></Link>
+            <div className="footer">
+              <p>&copy; 2020 InVision AR</p>
+            </div>
           </div>
         </div>
       </section>

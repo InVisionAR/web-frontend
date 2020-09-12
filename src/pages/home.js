3ac8c453f6cedd38cecf1ea02d41f5ba10f1d522
Rohan Kewalramani <rkewalramani4@gmail.com>
2020-09-12 14:49:18 -0400

    Fixed image not showing in project

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home.css";
-import logo from "./img/logo.png";
+import logo from "./img/logo-img.png";
 import { Link } from 'react-router-dom';
 
 export default function Home() {

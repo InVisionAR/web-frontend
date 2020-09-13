00027ef79234b1c6f8b53c20596233caf2c17dad
Rohan Kewalramani <rkewalramani4@gmail.com>
2020-09-12 22:16:30 -0400

    Fixed all reviewed changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./home.css";
 import logo from "./img/logo-img.png";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -10,12 +10,17 @@ export default function Home() {
         <div className="hero-body body">
           <div className="container">
             <img src={logo} alt="Logo" class="logo-img" />
-            <h1 className="title" id="title">InvisionAR</h1>
-            <p className="slogan">InVisioning the future...</p>
-            <Link to="/about"><button class="button is-primary" id="start-btn" to="#"><strong>GET STARTED</strong></button></Link>
-            <div className="footer">
-              <p>&copy; 2020 InVision AR</p>
-            </div>
+            <h1 className="title" id="title">
+              InvisionAR
+            </h1>
+            <p class="subtitle" className="slogan">
+              InVisioning the future...
+            </p>
+            <Link to="/about">
+              <button class="button is-primary" id="start-btn" to="#">
+                <strong>GET STARTED</strong>
+              </button>
+            </Link>
           </div>
         </div>
       </section>

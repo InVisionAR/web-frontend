a26b4f5918f92a6f230462ee5518ca975edf074a
Lint Action <lint-action@samuelmeuli.com>
2020-09-18 04:10:17 +0000

    Fix code style issues with Prettier

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -26,29 +26,31 @@ const NAQ = [
 export default function Quiz() {
   return (
     <section className="quiz-page">
-    <div className="container">
-      <h2 className="is-size-1 has-text-centered has-text-white">Time for a Quiz!</h2>
-      <div>
-        {MCQ.map((question) => {
-          return (
-            <MultipleChoice
-              questionTitle={question.questionTitle}
-              answerChoices={question.answerChoices}
-            />
-          );
-        })}
-      </div>
-      <div>
-        {NAQ.map((question) => {
-          return <NumericalAnswer questionTitle={question.questionTitle} />;
-        })}
-      </div>
-      {/* <div class="field has-text-centered submit-button">
+      <div className="container">
+        <h2 className="is-size-1 has-text-centered has-text-white">
+          Time for a Quiz!
+        </h2>
+        <div>
+          {MCQ.map((question) => {
+            return (
+              <MultipleChoice
+                questionTitle={question.questionTitle}
+                answerChoices={question.answerChoices}
+              />
+            );
+          })}
+        </div>
+        <div>
+          {NAQ.map((question) => {
+            return <NumericalAnswer questionTitle={question.questionTitle} />;
+          })}
+        </div>
+        {/* <div class="field has-text-centered submit-button">
         <div class="control">
           <button class="button is-link">Submit</button>
         </div>
       </div> */}
-    </div>
+      </div>
     </section>
   );
 }
